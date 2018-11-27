6522620396827e8545b560db9dbb1349a379cd18
BJ <bj@mac-mini.local>
2018-11-27 15:43:59 +0800

    Demo上传

---
+++ FaceDetect/FaceDetect/FaceViewController.h
@@ -0,0 +1,17 @@
+//
+//  FaceViewController.h
+//  IntelligentControl
+//
+//  Created by zhw_mac on 2018/11/25.
+//  Copyright © 2018 zhw_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FaceViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FaceDetect/FaceDetect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceDetect
+//
+//  Created by BJ on 2018/11/25.
+//  Copyright © 2018年 face. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
